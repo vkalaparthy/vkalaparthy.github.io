89770ac204e3907a7816928255fd4456670e270f
vkalaparthy <kalaparthy.vani@gmail.com>
2020-11-09 22:29:55 -0500

    Made changes to About me & readme

---
+++ src/pages/Contact.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../styles/common.css";
+import "../styles/contact.css";
+
+
+const Contact = () => (
+  <>
+    <h1 className="block-header">Contact</h1>
+      <hr className="custom-hr"></hr>
+      <form>
+        <div className="form-group">
+          <label htmlFor="formGroupExampleInput">Name</label>
+          <input type="text" className="form-control" id="formGroupExampleInput" placeholder="John Doe"></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="formGroupExampleInput2">Email</label>
+          <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="example@gmail.com"></input>
+        </div>
+        <div className="form-group">
+            <label htmlFor="exampleFormControlTextarea1">Message</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+        </div>
+      </form>
+      <button type="submit" className="btn custom-button">Submit</button>
+  </>
+);
+
+export default Contact;
